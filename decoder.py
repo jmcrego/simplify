60685e2dc5dd14d3ba1cff9328daa4a9e17cc348
jmcrego <jmcrego@gmail.com>
2018-10-22 16:12:37 +0200

    light

--- decoder.py
+++ decoder.py
@@ -37,13 +37,13 @@ class Attention(nn.Module):
         attn_energies = Variable(torch.zeros(B, S)) # [B, S]
         for b in range(B): #for all batches
             for j in range(S): # Calculate energy for each enc_output (referred to each source word)
-                attn_energies[b, j] = self.score(dec_hidden[b], enc_outputs[j, b])
+                attn_energies[b, j] = self.energy(dec_hidden[b], enc_outputs[j, b])
 
         # Normalize energies to weights in range 0 to 1
         attn_energies_norm = F.softmax(attn_energies, dim=1)
         return attn_energies_norm #[B, S]
 
-    def score(self, dec_hidden, enc_output):
+    def energy(self, dec_hidden, enc_output):
         #print("dec_hidden={}".format(dec_hidden.shape)) #[1, H]
         #print("enc_output={}".format(enc_output.shape)) #[1, H]
         if self.method == 'dot':
@@ -92,79 +92,70 @@ class DecoderRNN_Attn(nn.Module):
         ### these are the output vectors that will be filled at the end of the loop
         dec_output_words = torch.zeros([self.S - 1, self.B], dtype=torch.int32) #[S-1, B]
         dec_outputs = torch.zeros([self.S - 1, self.B, self.V], dtype=torch.float32) #[S-1, B, V]
-        ###
-        ### initialize dec_hidden (with dec_final) and attn_hidden (Eq 5 in Luong) used for input-feeding
-        ###
-        dec_hidden = self.init_state(enc_final) #[L, B, D*dim]
-        #dec_hidden is [layers x batch x (num_directions*dim)] #dim is hidden_size/2
-        #print("dec_hidden[0]={}".format(dec_hidden[0].shape)) ### len(dec_hidden)==num_layers
-        #print("dec_hidden[1]={}".format(dec_hidden[1].shape)) ### len(dec_hidden)==num_layers
-        attn_hidden = Variable(torch.zeros(1, self.B, self.H))
-
-        for t in range(self.S-1):
+        ### initialize dec_hidden (with dec_final)
+        dec_hidden = self.init_state(enc_final) #[L, B, D*dim] #dim is H/2
+        ### initialize attn_hidden (Eq 5 in Luong) used for input-feeding
+        attn_hidden = Variable(torch.zeros(1, self.B, self.H)) #[1, B, H]
+        for t in range(self.S-1): #loop to produce target words step by step
             ### decide which is the input word: consider teacher forcing
             if t==0: input_word = tgt_batch[t] ### it should be <ini>
             elif teacher_forcing < 1.0 and random.uniform() > teacher_forcing: input_word = dec_output_word #use t-1 predicted words
             else: input_word = tgt_batch[t] ### teacher forcing: the t-th words of each batch 
-            #print("input[t]={}".format(input_word))
             target_word = tgt_batch[t + 1] ### this is the reference for the words to be predicted
-            #print("intput[t+1]={}".format(target_word))
             dec_output, dec_hidden, attn_hidden, dec_attn = self.forward_step(input_word, attn_hidden, dec_hidden, enc_outputs)
-            #dec_output, dec_hidden = self.forward_step(input_word, dec_hidden)
+            ### get the 1-best
             top_val, dec_output_word = dec_output.topk(1) #dec_output_word is [batch_size, 1] (the best entry of each batch)   
             dec_output_word = dec_output_word.squeeze(1)
             ### add to final output vectors
-            dec_output_words[t] = dec_output_word #[t x batch_size]
-            dec_outputs[t] = dec_output #[t x batch_size x voc_size]
-            #print("dec_output_word[{}]={}".format(t,dec_output_word.shape))
+            dec_output_words[t] = dec_output_word #[t, B]
+            dec_outputs[t] = dec_output #[t, B, V]
 
         return dec_outputs, dec_output_words
 
     def forward_step(self, input_word, attn_hidden, dec_hidden, enc_outputs):
-        ### input_word [batch_size] is the target word at instance t for all batches 
-        #print("forward_step")
-        #print("input_word={}".format(input_word.shape))
-        #print("attn_hidden={}".format(attn_hidden.shape))
-        #print("dec_hidden={}".format(dec_hidden[0].shape)) #[num_layers, batch_size, hidden_size], 
-        #print("enc_outputs={}".format(enc_outputs.shape)) #[seq_size, batch_size, hidden_size]
-        #print("dec_hidden[0]={}".format(dec_hidden[0].shape)) ### [num_layers, batch_size, hidden_size]     len(dec_hidden)==num_layers
-        #print("dec_hidden[1]={}".format(dec_hidden[1].shape)) ### [num_layers, batch_size, hidden_size]     len(dec_hidden)==num_layers
-        # get the embedding of the current input word (last output word)
+        #print("input_word={}".format(input_word.shape))    #[B] previous target word 
+        #print("attn_hidden={}".format(attn_hidden.shape))  #[1, B, H] previous attn_hidden
+        #print("dec_hidden[0]={}".format(dec_hidden[0].shape)) #[L, B, H] previous dec_hidden 
+        #print("dec_hidden[1]={}".format(dec_hidden[1].shape)) #[L, B, H] previous dec_hidden 
+        #print("enc_outputs={}".format(enc_outputs.shape))  #[S, B, H] encoder outputs
+        # get the embedding of the current input word (is the previous target word)
         input_emb = self.embedding(torch.tensor(input_word))
-        input_emb = self.dropout(input_emb) #[batch_size, emb_size]
-        input_emb = input_emb.unsqueeze(0) # [1, batch_size, emb_size]
-        #print("input_emb={}".format(input_emb.shape)) 
-        #concat input_emb; attn_hidden for input-feeding
+        input_emb = self.dropout(input_emb) #[B, E]
+        input_emb = input_emb.unsqueeze(0) # [1, B, E]
+        #input feeding: input_emb + attn_hidden
         input_emb_attn = torch.cat((input_emb, attn_hidden), 2)
-
+        #rnn layer
         rnn_output, dec_hidden = self.rnn(input_emb_attn, dec_hidden)
+        rnn_output = rnn_output.squeeze(0) # [1, B, H] -> [B, H]
+        #print("rnn_output={}".format(rnn_output.shape)) #[B, H]
+        #print("dec_hidden[0]={}".format(dec_hidden[0].shape)) #[L, B, H] (h)
+        #print("dec_hidden[1]={}".format(dec_hidden[1].shape)) #[L, B, H] (c)
 
-        rnn_output = rnn_output.squeeze(0) # S=1 x B x H -> B x H
-        #print("rnn_output={}".format(rnn_output.shape))
-        #print("dec_hidden[0]={}".format(dec_hidden[0].shape))
-        #print("dec_hidden[1]={}".format(dec_hidden[1].shape))
-        
-        # Calculate attention from current last layer [-1] RNN h state [0] and all encoder outputs;
+        # to calculate attention weights for each encoder output
+        # we consider the last layer [-1] h state [0] of dec_hidden => dec_hidden[0][-1]
+        # and all encoder outputs => dec_outputs
         # apply to encoder outputs to get weighted average
         last_h_state = dec_hidden[0][-1]
-        attn_weights = self.attn(last_h_state, enc_outputs) ## attn weights for each encoder output
-        attn_weights = attn_weights.unsqueeze(0) #resize to [1, batch_size, seq_length]
-        #print("attn_weights={}".format(attn_weights.shape)) #[1, batch_size, seq_length]
-        #print("enc_outputs={}".format(enc_outputs.shape)) #[seq_size, batch_size, hidden_size]
-        context = attn_weights.transpose(1,0).bmm(enc_outputs.transpose(1, 0)) # batched multiplication [batch_size, 1, seq_length] x [batch_size, seq_length, hidden_size] = [batch_size, 1, hidden_size]
-        context = context.squeeze(1)  #[batch_size, hidden_size]
+        attn_weights = self.attn(dec_hidden[0][-1], enc_outputs) # [B, S]
+        attn_weights = attn_weights.unsqueeze(0) # [1, B, S]
+        #print("attn_weights={}".format(attn_weights.shape)) #[1, B, S]
+        context = attn_weights.transpose(1,0).bmm(enc_outputs.transpose(1, 0)) # batched multiplication [B, 1, S] x [B, S, H] => [B, 1, H]
+        context = context.squeeze(1)  #[B, H]
         #print('context={}'.format(context.shape))
-        # concatenate together rnn_output and context and apply layer (Luong eq. 5)
-        cat_output_context = torch.cat((rnn_output, context), 1)
-        #print('concat_output_context={}'.format(cat_output_context.shape))
-        attn_hidden = torch.tanh(self.concat(cat_output_context)) #### this is what should be use for input-feed
+        # concatenate together rnn_output and context and apply concat layer (Luong eq. 5)
+        cat_rnnoutput_context = torch.cat((rnn_output, context), 1) # [B, 2*H]
+        #print('cat_rnnoutput_context={}'.format(cat_rnnoutput_context.shape)) 
+        attn_hidden = torch.tanh(self.concat(cat_rnnoutput_context)) #[B, H] applied concat layer 
         #print('attn_hidden={}'.format(attn_hidden.shape)) #[B, H]
-        # Finally predict next token (Luong eq. 6)
-        dec_output = self.output(attn_hidden)
+        # aply output layer (Luong eq. 6)
+        dec_output = self.output(attn_hidden) # [B, V]
         #print("dec_output={}".format(dec_output.shape))
-        dec_output = F.log_softmax(dec_output, dim=1)
+        # apply softmax layer
+        dec_output = F.log_softmax(dec_output, dim=1) #[B, V]
         #print("dec_output={}".format(dec_output.shape))
-        attn_hidden = attn_hidden.unsqueeze(0) #[1, B, H]
+        attn_hidden = attn_hidden.unsqueeze(0) #[B, H] => [1, B, H] (vector used for input-feeding)
+        #print("attn_hidden={}".format(attn_hidden.shape))
+
         return dec_output, dec_hidden, attn_hidden, attn_weights
 
     def init_state(self, encoder_hidden):
@@ -176,13 +167,9 @@ class DecoderRNN_Attn(nn.Module):
         return encoder_hidden
 
     def cat_directions(self, h):
-        #if num_directions is 1 (not a bidirectional encoder) there is nothing to do
+        #if D is 1 (not a bidirectional encoder) there is nothing to do
         if self.D == 1: return h
-        #otherwise, h is:        
-        #[(layers*num_directions) x batch_size x dim] 
-        # and h should be:
-        #[layers x batch x (num_directions*dim)] 
-        #dim is hidden_size/2
+        #otherwise, h is: [L*D, B, dim] and h should be: [L, B, D*dim] and dim is H/2
         h = torch.cat([h[0:h.size(0):2], h[1:h.size(0):2]], 2)
         return h
 
