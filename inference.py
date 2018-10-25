98404a1694e362bbef5d1e3d1fa55cbdcf224384
jmcrego <jmcrego@gmail.com>
2018-10-25 16:35:13 +0200

    time logs

--- inference.py
+++ inference.py
@@ -12,6 +12,7 @@ from utils import print_time
 class Inference(): 
 
     def __init__(self, cfg, mod, tst):
+        self.cfg = cfg
         ini_time = time.time()
         print_time('Start TEST')
         for val_iter, (src_batch, tgt_batch, ref_batch, raw_src_batch, raw_tgt_batch, len_src_batch, len_tgt_batch) in enumerate(tst.minibatches()):
@@ -19,16 +20,16 @@ class Inference():
                 src_batch = src_batch.cuda()
                 tgt_batch = tgt_batch.cuda()
             _, predict_batch = mod(src_batch, tgt_batch, len_src_batch, len_tgt_batch) ### forward
-            self.display(mod, predict_batch)
+            self.display(predict_batch)
 
         seconds = "{:.2f}".format(time.time() - ini_time)
         print_time('End of TEST seconds={})\n'.format(seconds))
 
 
-    def display(self, mod, predict_batch):
+    def display(self, predict_batch):
         for predict_sentence in predict_batch:
             sentence = []
             for word_id in predict_sentence:
-                word = cfg.tvoc.get(int(word_id))
+                word = self.cfg.tvoc.get(int(word_id))
                 sentence.append("{}".format(word))
             print(sentence)
