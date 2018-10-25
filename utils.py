a7c813d93d8d114601ec6685439de7726b372bbf
jmcrego <jmcrego@gmail.com>
2018-10-25 12:00:27 +0200

    time logs

--- utils.py
+++ utils.py
@@ -3,8 +3,10 @@
 import sys
 import os
 import time
+import torch
 
 def print_time(desc):
+	torch.cuda.synchronize()
 #    curr_time = time.strftime("[%Y-%m-%d_%X]", time.localtime())
 #    sys.stdout.write('{} {}\n'.format(curr_time,desc))
     curr_milli = int(round(time.time() * 1000))
