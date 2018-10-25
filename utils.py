9d4e292f3b8c007bdfad4f4d001bf139cf04c1c4
jmcrego <jmcrego@gmail.com>
2018-10-25 12:02:08 +0200

    time logs

--- utils.py
+++ utils.py
@@ -7,8 +7,8 @@ import torch
 
 def print_time(desc):
 	torch.cuda.synchronize()
-	#    curr_time = time.strftime("[%Y-%m-%d_%X]", time.localtime())
-	#    sys.stdout.write('{} {}\n'.format(curr_time,desc))
-    curr_milli = int(round(time.time() * 1000))
-    sys.stdout.write('[{}] {}\n'.format(curr_milli,desc))
+	#curr_time = time.strftime("[%Y-%m-%d_%X]", time.localtime())
+	#sys.stdout.write('{} {}\n'.format(curr_time,desc))
+   	curr_milli = int(round(time.time() * 1000))
+   	sys.stdout.write('[{}] {}\n'.format(curr_milli,desc))
 
