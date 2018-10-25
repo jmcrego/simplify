18c27a43cdbde58b3b38a0526d6e7b61b0b4f89d
jmcrego <jmcrego@gmail.com>
2018-10-25 22:40:39 +0200

    time logs

--- checkpoint.py
+++ checkpoint.py
@@ -11,13 +11,12 @@ from utils import print_time
 class Checkpoint(object):
 
     def __init__(self, path=None):
-        if path is not None:
+        if path is not None: 
             self.path = path
             self.contains_model = self.contains_model()
 
     def save(self, cfg, mod, opt, loss):
         if not os.path.exists(self.path): os.makedirs(self.path)
-#        date_time = time.strftime('%Y-%m-%d-%H:%M:%S', time.localtime())
         date_time = time.strftime('%Y%m%d-%H%M%S', time.localtime())
         checkpoint = os.path.join(self.path, 'checkpoint_{}_{:0>6}_{}.pt'.format(date_time,cfg.n_iters_sofar,"{:.5f}".format(loss)[0:7]))
         chk = {'mod': mod.state_dict(), 'opt': opt.state_dict(), 'cfg': cfg}
@@ -25,12 +24,12 @@ class Checkpoint(object):
         print_time("Saved checkpoint [{}]".format(checkpoint))
 
     def load(self, file=None):
-        if file is None:
+        if file is None: ### load the most recent checkpoint in self.path
             if not os.path.exists(self.path): sys.exit('error: no experiments found in {}'.format(self.path))
             all_saves = sorted(glob.glob(self.path+'/checkpoint_*.pt'), reverse=True)
             if len(all_saves)==0: sys.exit('error: no checkpoint found in dir={}'.format(self.path))
             checkpoint = all_saves[0]
-        else: 
+        else: ### load the checkpoint given in file
             if not os.path.exists(file): sys.exit('error: no checkpoint found in {}'.format(file))
             checkpoint = file
         chk = torch.load(checkpoint) 
