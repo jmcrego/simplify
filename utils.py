fe104ea16c1daf02bd65a0acd7f5dfec22236666
jmcrego <jmcrego@gmail.com>
2018-10-25 11:52:21 +0200

    time logs

--- utils.py
+++ utils.py
@@ -5,6 +5,8 @@ import os
 import time
 
 def print_time(desc):
-    curr_time = time.strftime("[%Y-%m-%d_%X]", time.localtime())
-    sys.stdout.write('{} {}\n'.format(curr_time,desc))
+#    curr_time = time.strftime("[%Y-%m-%d_%X]", time.localtime())
+#    sys.stdout.write('{} {}\n'.format(curr_time,desc))
+    curr_milli = int(round(time.time() * 1000))
+    sys.stdout.write('[{}] {}\n'.format(curr_milli,desc))
 
