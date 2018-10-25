7fc6156a5904cb08c687d06c2b898202815e71ff
jmcrego <jmcrego@gmail.com>
2018-10-25 14:45:57 +0200

    time logs

--- data.py
+++ data.py
@@ -173,6 +173,7 @@ class Dataset():
                 self.ntgt += 1
             yield isrc, itgt, src, tgt ### return for iterator
             nsent += 1
+        print('finished dataset pairs')
 
     def minibatches(self):
         minibatch_size = self.batch_size
